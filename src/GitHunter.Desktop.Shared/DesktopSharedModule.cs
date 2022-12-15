c5694b9f8c559748d4bb47708f7f9bdb61a46d72
Salih <salihozkara00@gmail.com>
2022-12-15 22:51:03 +0300

    Added DesktopCoreSharedModule

--- src/GitHunter.Desktop.Shared/DesktopSharedModule.cs
+++ src/GitHunter.Desktop.Shared/DesktopSharedModule.cs
@@ -1,10 +1,14 @@
 ﻿using GitHunter.Application;
 using GitHunter.Core.Modules;
+using GitHunter.Desktop.Core;
 using Volo.Abp.Modularity;
 
 namespace GitHunter.Desktop;
 
-[DependsOn(typeof(ApplicationSharedModule))]
+[DependsOn(
+    typeof(ApplicationSharedModule),
+    typeof(DesktopCoreSharedModule)
+    )]
 public class DesktopSharedModule : GitHunterModule
 {
 }
