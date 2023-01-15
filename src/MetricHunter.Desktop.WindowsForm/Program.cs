bd9169e50a316bdad75c70cbf45ffffaa7140b4c
Salih <salihozkara00@gmail.com>
2023-01-15 22:08:38 +0300

    Add Desktop Log

--- src/MetricHunter.Desktop.WindowsForm/Program.cs
+++ src/MetricHunter.Desktop.WindowsForm/Program.cs
@@ -1,4 +1,5 @@
 using MetricHunter.Core.Processes;
+using MetricHunter.Desktop.Logs;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 using Serilog.Events;
@@ -8,19 +9,20 @@ namespace MetricHunter.Desktop;
 
 public class Program
 {
+
     [STAThread]
     public static void Main(string[] args)
     {
         ApplicationConfiguration.Initialize();
-
+        
         Log.Logger = new LoggerConfiguration()
             .MinimumLevel.Debug()
             .MinimumLevel.Override("Volo.Abp", LogEventLevel.Warning)
             .Enrich.FromLogContext()
             .WriteTo.File(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs/logs.txt"))
             .WriteTo.Console(outputTemplate: "{Message:lj}{NewLine}{Exception}")
+            .WriteTo.Desktop()
             .CreateLogger();
-
 
         using var application = AbpApplicationFactory.Create<DesktopWindowsFormModule>(
             options =>
