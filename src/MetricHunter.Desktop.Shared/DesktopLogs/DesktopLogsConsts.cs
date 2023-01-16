a83803635fb3d7d9893ffc6650f130c8b5e6de04
Salih <salihozkara00@gmail.com>
2023-01-16 19:51:35 +0300

    Update DesktopLogsConsts.cs

---
+++ src/MetricHunter.Desktop.Shared/DesktopLogs/DesktopLogsConsts.cs
@@ -0,0 +1,9 @@
+﻿using MetricHunter.Core.Paths;
+using MetricHunter.Core.Strings;
+
+namespace MetricHunter.Desktop.DesktopLogs;
+
+public class DesktopLogsConsts
+{
+    public static readonly FilePath LogFilePath = AppDomain.CurrentDomain.BaseDirectory + "Logs\\MetricHunter.log".ToFilePath();
+}
